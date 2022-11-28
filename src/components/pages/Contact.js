841a878c5c97d410627cfd837ff9695757abf495
Garrett Jackson <garrettjackson43@gmail.com>
2022-11-28 14:38:11 -0500

    inital commit with minor updates

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Email: garrettjackson43@gmail.com 
+        Cell: 203-535-2543
+      </p>
+    </div>
+  );
+}
